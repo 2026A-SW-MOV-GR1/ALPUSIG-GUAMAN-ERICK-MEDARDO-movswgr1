19bbafc4839d787195a80ddf5ffc804dd69f407a
ElErick-MG <erick.alpusig@epn.edu.ec>
2026-05-19 23:59:05 -0500

    fix: Correct import statement formatting in AppScreen.kt

--- capitulo01/shared/src/commonMain/kotlin/com/example/moviles2026aswgr1/shared/AppScreen.kt
+++ capitulo01/shared/src/commonMain/kotlin/com/example/moviles2026aswgr1/shared/AppScreen.kt
@@ -1,7 +1,6 @@
 package com.example.moviles2026aswgr1.shared
 
-import
-androidx.compose.foundation.background
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
